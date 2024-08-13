04ca3d428e06b3c5cc6a8980207507173a05721b
yushanlu <yushanlu1031@gmail.com>
2024-08-13 19:16:28 +0800

    Loading Page Done

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,6 +1,7 @@
 import EventsList from "@/components/events-list"
 import H1 from "@/components/h1"
 import { EventoEvent } from "@/lib/types"
+import { sleep } from "@/lib/util"
 
 type EventsPageProps = {
   params: {
@@ -10,6 +11,7 @@ type EventsPageProps = {
 
 export default async function EventsPage({params}: EventsPageProps) {
   const city = params.city;
+  // await sleep(2000);
   const response = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`);
   const events: EventoEvent[] = await response.json()
   
