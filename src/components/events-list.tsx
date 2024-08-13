8c291a54b8d512db3b6b7314540d9dc3879d17fd
yushanlu <yushanlu1031@gmail.com>
2024-08-13 23:59:28 +0800

    Changing cache to revalidate every 5 minutes

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -10,7 +10,11 @@ export default async function EventsList({
   city} : EventListProps) {
 
   // await sleep(2000);
-  const response = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`, {cache: "no-cache"}) ;
+  const response = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`, {
+    next: {
+      revalidate: 300, 
+    }
+  })  ;
   const events: EventoEvent[] = await response.json()
   
   return <section className="flex flex-wrap gap-10 justify-center px-[20px]">
