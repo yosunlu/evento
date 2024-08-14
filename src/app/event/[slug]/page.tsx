406049bb5ba29a5818762639d016fe623fdd3ee9
yushanlu <yushanlu1031@gmail.com>
2024-08-15 00:59:16 +0800

    Add generateStaticParam to make popular routes static

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -17,6 +17,15 @@ export async function generateMetadata( {params} : Props ) : Promise <Metadata> 
   }
 }
 
+export async function generateStaticParams() {
+    return [{
+      slug: 'comedy-extravaganza'
+    },{
+      slug: 'dj-practice-session'
+    }
+  ]
+}
+
 export default async function EventPage({params}: Props) {
   const slug = params.slug;
   const event = await getEvent(slug);
